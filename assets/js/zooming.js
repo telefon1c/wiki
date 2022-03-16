5e88bf8563bebe91545f2fa4c1f61f889512ba19
Retype GitHub Action <retype@object.net>
2022-03-16 16:23:21 +0000

    Refreshes Retype-generated documentation.
    Process triggered by jorikfon.

--- assets/js/zooming.js
+++ assets/js/zooming.js
@@ -1,32 +1,30 @@
-
 <!-- github.com/kingdido999/zooming -->
-
-if (window.innerWidth > 500) {
-	document.addEventListener("turbo:load", function() {
-		head.ready(document, function() {
-			head.load('https://cdnjs.cloudflare.com/ajax/libs/zooming/2.1.1/zooming.js', function() {
+document.addEventListener("turbo:load", function () {
+	head.ready(document, function () {
+		// Отключим зумм для мобильных
+		if (window.innerWidth > 500) {
+			head.load('https://cdnjs.cloudflare.com/ajax/libs/zooming/2.1.1/zooming.js', function () {
 				var zooming = new Zooming();
 				zooming.listen('.img-zoomable');
+			})
+		}
+		head.load('https://unpkg.com/freezeframe/dist/freezeframe.min.js', function () {
+			// Custom options
+			new Freezeframe({
+				selector: '.play-on-hover',
+				trigger: 'hover',
+				overlay: true,
+				responsive: true,
+				warnings: false
 			});
-			head.load('https://unpkg.com/freezeframe/dist/freezeframe.min.js', function() {
-				// Custom options
-				new Freezeframe({
-					selector: '.play-on-hover',
-					trigger: 'hover',
-					overlay: true,
-					responsive: true,
-					warnings: false
-				});
-				new Freezeframe({
-					selector: '.play-on-click',
-					trigger: 'click',
-					overlay: true,
-					responsive: true,
-					warnings: false
-				});
-			});
-		});
+			new Freezeframe({
+				selector: '.play-on-click',
+				trigger: 'click',
+				overlay: true,
+				responsive: true,
+				warnings: false
+			})
+		})
 	})
-}
-
+});
 <!-- End github.com/kingdido999/zooming -->
--- resources/js/retype.js
+++ resources/js/retype.js
@@ -34,37 +34,35 @@ document.addEventListener("turbo:load", function() {
 		GTMINIT(window, document,'script','dataLayer','GTM-KXBHF8J');
 		return true;
 	})
-})
-<!-- github.com/kingdido999/zooming -->
-
-if (window.innerWidth > 500) {
-	document.addEventListener("turbo:load", function() {
-		head.ready(document, function() {
-			head.load('https://cdnjs.cloudflare.com/ajax/libs/zooming/2.1.1/zooming.js', function() {
+})<!-- github.com/kingdido999/zooming -->
+document.addEventListener("turbo:load", function () {
+	head.ready(document, function () {
+		// Отключим зумм для мобильных
+		if (window.innerWidth > 500) {
+			head.load('https://cdnjs.cloudflare.com/ajax/libs/zooming/2.1.1/zooming.js', function () {
 				var zooming = new Zooming();
 				zooming.listen('.img-zoomable');
+			})
+		}
+		head.load('https://unpkg.com/freezeframe/dist/freezeframe.min.js', function () {
+			// Custom options
+			new Freezeframe({
+				selector: '.play-on-hover',
+				trigger: 'hover',
+				overlay: true,
+				responsive: true,
+				warnings: false
 			});
-			head.load('https://unpkg.com/freezeframe/dist/freezeframe.min.js', function() {
-				// Custom options
-				new Freezeframe({
-					selector: '.play-on-hover',
-					trigger: 'hover',
-					overlay: true,
-					responsive: true,
-					warnings: false
-				});
-				new Freezeframe({
-					selector: '.play-on-click',
-					trigger: 'click',
-					overlay: true,
-					responsive: true,
-					warnings: false
-				});
-			});
-		});
+			new Freezeframe({
+				selector: '.play-on-click',
+				trigger: 'click',
+				overlay: true,
+				responsive: true,
+				warnings: false
+			})
+		})
 	})
-}
-
+});
 <!-- End github.com/kingdido999/zooming -->
 /*! Retype v1.11.2 | retype.com | Copyright 2022. Object.NET, Inc. All rights reserved. */
 
