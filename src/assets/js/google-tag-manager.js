e6fc660ad89671245408afa0c133fd14014bbc6f
jorikfon <nbek@miko.ru>
2022-05-23 21:20:51 +0300

    Правлю автопроигрыватель

--- src/assets/js/google-tag-manager.js
+++ src/assets/js/google-tag-manager.js
@@ -1,17 +1,17 @@
-
-document.addEventListener("turbo:load", function() {
-	head.ready(document, function() {
-		function GTMINIT(w,d,s,l,i) {
-			w[l]=w[l]||[];
-			w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-			var f=d.getElementsByTagName(s)[0],
-				j=d.createElement(s),
-				dl=l!='dataLayer'?'&l='+l:'';
-			j.async=true;
-			j.src=	'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-			f.parentNode.insertBefore(j,f);
-		}
-		GTMINIT(window, document,'script','dataLayer','GTM-KXBHF8J');
-		return true;
-	})
-})
+//
+// document.addEventListener("turbo:load", function() {
+// 	head.ready(document, function() {
+// 		function GTMINIT(w,d,s,l,i) {
+// 			w[l]=w[l]||[];
+// 			w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+// 			var f=d.getElementsByTagName(s)[0],
+// 				j=d.createElement(s),
+// 				dl=l!='dataLayer'?'&l='+l:'';
+// 			j.async=true;
+// 			j.src=	'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+// 			f.parentNode.insertBefore(j,f);
+// 		}
+// 		GTMINIT(window, document,'script','dataLayer','GTM-KXBHF8J');
+// 		return true;
+// 	})
+// })
