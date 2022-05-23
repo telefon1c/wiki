c3f151560472967a2384774f029e426f66b0caa0
Retype GitHub Action <retype@object.net>
2022-05-23 18:38:02 +0000

    Refreshes Retype-generated documentation.
    Process triggered by jorikfon.

--- assets/js/freezeframe.js
+++ assets/js/freezeframe.js
@@ -1,40 +1,24 @@
-// document.addEventListener("turbo:load", function () {
-// 	head.ready(document, function () {
-// 		head.load('https://unpkg.com/freezeframe/dist/freezeframe.min.js', function () {
-// 			// Custom options
-// 			new Freezeframe({
-// 				selector: '.play-on-hover',
-// 				trigger: 'hover',
-// 				overlay: true,
-// 				responsive: true,
-// 				warnings: false
-// 			});
-// 			new Freezeframe({
-// 				selector: '.play-on-click',
-// 				trigger: 'click',
-// 				overlay: true,
-// 				responsive: true,
-// 				warnings: false
-// 			})
-// 		})
-// 	})
-// });
-
-document.addEventListener("DOMContentLoaded", function(event) {
-	// Custom options
-	new Freezeframe({
-		selector: '.play-on-hover',
-		trigger: 'hover',
-		overlay: true,
-		responsive: true,
-		warnings: false
-	});
-	new Freezeframe({
-		selector: '.play-on-click',
-		trigger: 'click',
-		overlay: true,
-		responsive: true,
-		warnings: false
+document.addEventListener("turbo:load", function () {
+	head.ready(document, function () {
+		head.load('https://unpkg.com/freezeframe/dist/freezeframe.min.js', function () {
+			// Custom options
+			new Freezeframe({
+				selector: '.play-on-hover',
+				trigger: 'hover',
+				overlay: true,
+				responsive: true,
+				warnings: false
+			});
+			new Freezeframe({
+				selector: '.play-on-click',
+				trigger: 'click',
+				overlay: true,
+				responsive: true,
+				warnings: false
+			})
+		})
 	})
 });
+
+
 
--- assets/js/zooming.js
+++ assets/js/zooming.js
@@ -1,7 +1,7 @@
 <!-- github.com/kingdido999/zooming -->
 document.addEventListener("turbo:load", function () {
 	head.ready(document, function () {
-		// Отключим зумм для мобильных
+		// Отключим зум для мобильных
 		if (window.innerWidth > 500) {
 			head.load('https://cdnjs.cloudflare.com/ajax/libs/zooming/2.1.1/zooming.js', function () {
 				var zooming = new Zooming();
