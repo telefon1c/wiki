524b4c27d86b73742a6b73bc5100f99d413c8c6b
Retype GitHub Action <retype@object.net>
2022-05-23 18:02:04 +0000

    Refreshes Retype-generated documentation.
    Process triggered by jorikfon.

---
+++ assets/js/freezeframe.js
@@ -0,0 +1,37 @@
+// document.addEventListener("turbo:load", function () {
+// 	head.ready(document, function () {
+// 		head.load('https://unpkg.com/freezeframe/dist/freezeframe.min.js', function () {
+// 			// Custom options
+// 			new Freezeframe({
+// 				selector: '.play-on-hover',
+// 				trigger: 'hover',
+// 				overlay: true,
+// 				responsive: true,
+// 				warnings: false
+// 			});
+// 			new Freezeframe({
+// 				selector: '.play-on-click',
+// 				trigger: 'click',
+// 				overlay: true,
+// 				responsive: true,
+// 				warnings: false
+// 			})
+// 		})
+// 	})
+// });
+
+// Custom options
+new Freezeframe({
+	selector: '.play-on-hover',
+	trigger: 'hover',
+	overlay: true,
+	responsive: true,
+	warnings: false
+});
+new Freezeframe({
+	selector: '.play-on-click',
+	trigger: 'click',
+	overlay: true,
+	responsive: true,
+	warnings: false
+})
