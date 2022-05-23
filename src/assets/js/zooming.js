5519c4f314927f8a910076cc3f983b115c3cedf1
jorikfon <nbek@miko.ru>
2022-05-23 21:00:12 +0300

    Правлю автопроигрыватель

--- src/assets/js/zooming.js
+++ src/assets/js/zooming.js
@@ -8,23 +8,6 @@ document.addEventListener("turbo:load", function () {
 				zooming.listen('.img-zoomable');
 			})
 		}
-		head.load('https://unpkg.com/freezeframe/dist/freezeframe.min.js', function () {
-			// Custom options
-			new Freezeframe({
-				selector: '.play-on-hover',
-				trigger: 'hover',
-				overlay: true,
-				responsive: true,
-				warnings: false
-			});
-			new Freezeframe({
-				selector: '.play-on-click',
-				trigger: 'click',
-				overlay: true,
-				responsive: true,
-				warnings: false
-			})
-		})
 	})
 });
 <!-- End github.com/kingdido999/zooming -->
